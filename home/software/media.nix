f315063966dd0efa53f858b5aac230f5d18225fa
Kyer Potts <kyerpotts@gmail.com>
2023-08-13 20:49:54 +0800

    Set up spotify-tui

--- home/software/media.nix
+++ home/software/media.nix
@@ -12,6 +12,7 @@
     imv
 
     # spotify
+    spotify-tui
   ];
 
   programs = {
@@ -22,5 +23,24 @@
     };
   };
 
-  services = { playerctld.enable = true; };
+  services = {
+    playerctld.enable = true;
+
+    spotifyd = {
+      enable = true;
+      package = pkgs.spotifyd.override { withMpris = true; };
+      settings.global = {
+        autoplay = true;
+        backend = "pulseaudio";
+        bitrate = 320;
+        cache_path = "${config.xdg.cacheHome}/spotifyd";
+        device_type = "computer";
+        initial_volume = "100";
+        password_cmd = "tail -1 /run/agenix/spotify";
+        use_mpris = true;
+        username_cmd = "head -1 /run/agenix/spotify";
+        volume_normalisation = false;
+      };
+    };
+  };
 }
