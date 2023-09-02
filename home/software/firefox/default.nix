dc8d398e20344e5b740a1edac81808dcacf0145a
Kyer Potts <kyerpotts@gmail.com>
2023-09-02 18:44:56 +0800

    Implementing timezone fix for firefox

--- home/software/firefox/default.nix
+++ home/software/firefox/default.nix
@@ -1,2 +1,16 @@
-{ pkgs, config, ... }: {
+{ pkgs, ... }:
+let
+  patched-firefox = pkgs.symlinkJoin {
+          name= "patched-firefox";
+          paths = [ pkgs.firefox ];
+          buildInputs = [ pkgs.makeWrapper ];
+          postBuild = ''
+                  wrapProgram "$out/bin/firefox" --set TZ /etc/localtime
+          '';
+  };
+in
+{
+  home.packages = with pkgs; [
+          patched-firefox
+  ];
 }
